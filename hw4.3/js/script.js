1d3031e60e4c24901b9639c35525371773cbe99e
dimove9 <dimaovechkin94@gmail.com>
2018-11-17 19:38:51 +0300

    hw4.3

---
+++ hw4.3/js/script.js
@@ -0,0 +1,8 @@
+ console.log('You are  at  '+window.location);
+
+ let number = prompt('Введите число');
+ number = parseInt(number);
+
+
+ (number>=0) ? console.log(number) : console.log(number);
+
